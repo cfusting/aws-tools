5a7bb6ac1385c897b4f31535b673de3b33f622b9
Chris Fusting <cfusting@gmail.com>
2017-10-19 14:54:28 -0400

    Small fixes

--- launch.sh
+++ launch.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
+# Assumes you have commands in .bash_aliases that require EC2NODE.
+
 connd() {
   export EC2NODE=$(python ~/bunny/aws-tools/launch.py -t $1 $2)
-  alias node="ssh -i ~/aws/key-pairs/cfusting-key-pair-n-virginia.pem ubuntu@${EC2NODE}"
+  source $HOME/.bash_aliases
 }
 
 congp() {
-  export EC2NODE=$(python ~/bunny/aws-tools/launch.py -t g3.4xlarge 1.20)
-  alias node="ssh -i ~/aws/key-pairs/cfusting-key-pair-n-virginia.pem ubuntu@${EC2NODE}"
+  export EC2NODE=$(python ~/bunny/aws-tools/launch.py -t g3.4xlarge -p 1.20)
+  source $HOME/.bash_aliases
 }
 
 concp() {
-  export EC2NODE=$(python ~/bunny/aws-tools/launch.py -t m4.16xlarge 3.30)
-  alias node="ssh -i ~/aws/key-pairs/cfusting-key-pair-n-virginia.pem ubuntu@${EC2NODE}"
+  export EC2NODE=$(python ~/bunny/aws-tools/launch.py -t c4.8xlarge -p .6)
+  source $HOME/.bash_aliases
 }
 
